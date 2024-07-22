c33e7153bde6c1d1f6f5474af7cbc3dfffe54e77
Janilson Varela <janilsonjvs@gmail.com>
2024-07-22 20:51:35 -0300

    Initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title', env('APP_NAME'))
+
+@section('content_header')
+
+@stop
+
+@section('content')
+
+
+@stop
+@section('js')
+
+@stop
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
