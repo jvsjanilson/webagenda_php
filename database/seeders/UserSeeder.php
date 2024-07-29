92263759c7b4f1520116c9e58bc962d26106cab6
Janilson Varela <janilsonjvs@gmail.com>
2024-07-28 22:19:48 -0300

    feat: add ico

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,11 +14,11 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            ['name' => 'Adminsistrador',  'email'=> 'contato@destakmoveisecolchoes.com.br', 'password' => bcrypt('admin123'), 'superuser' => 1 ], //destak
-            ['name' => 'Vendedor Destak',  'email'=> 'spindustriaecomercio@gmail.com', 'password' => bcrypt('vendedor123'), 'superuser' => 0 ], //destak
-            ['name' => 'Ziglar',  'email'=> 'ziglarmoveis@gmail.com', 'password' => bcrypt('ziglar123'), 'superuser' => 0 ], //ziglar
-            ['name' => 'Sono e Conforto',  'email'=> 'sonoeconfortorn@hotmail.com', 'password' => bcrypt('sonoc123'), 'superuser' => 0 ], //sono conforto
-            ['name' => 'Sono Prime',  'email'=> 'sonoeconfortoprime@hotmail.com ', 'password' => bcrypt('sonop123'), 'superuser' => 0 ], //sono prime
+            ['name' => 'Administrador',  'email'=> 'contato@destakmoveisecolchoes.com.br', 'password' => bcrypt('@admin123'), 'superuser' => 1 ], //destak
+            ['name' => 'Vendedor Destak',  'email'=> 'spindustriaecomercio@gmail.com', 'password' => bcrypt('@vendedor123'), 'superuser' => 0 ], //destak
+            ['name' => 'Ziglar',  'email'=> 'ziglarmoveis@gmail.com', 'password' => bcrypt('@ziglar123'), 'superuser' => 0 ], //ziglar
+            ['name' => 'Sono e Conforto',  'email'=> 'sonoeconfortorn@hotmail.com', 'password' => bcrypt('@sonoc123'), 'superuser' => 0 ], //sono conforto
+            ['name' => 'Sono Prime',  'email'=> 'sonoeconfortoprime@hotmail.com ', 'password' => bcrypt('@sonop123'), 'superuser' => 0 ], //sono prime
         ]);
     }
 }
